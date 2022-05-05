6d4a94f0850df0c41f7e3ef78df13645dca6cdd6
SUJIN <cainstear@gmail.com>
2022-05-05 22:18:51 +0900

    22/05/05 코딩테스트 공부

---
+++ coding_test/src/level_bronze/Q_1356.java
@@ -0,0 +1,28 @@
+package level_bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_1356 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a, b, i, j, sz;
+		String str = br.readLine();
+		boolean chk = false;
+		sz = str.length();
+		for (i = 0; i < sz - 1; i++) {
+			a = b = 1;
+			for (j = 0; j <= i; j++)
+				a *= str.charAt(j) - '0';
+			for (j = i + 1; j < sz; j++)
+				b *= str.charAt(j) - '0';
+			if (a == b) {
+				chk = true;
+				break;
+			}
+		}
+		System.out.println(chk ? "YES" : "NO");
+	}
+}
